1dea2bc0da79963c93d60c3953a002c8b115c7da
luissalvado <luismiguelsalvado@gmail.com>
2020-10-09 16:15:12 +0100

    config a module.

---
+++ app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './../../../web-dev-learning/Angular/01-Learning_Angular/01-Using_Angular_CLI/learnangular5/src/app/app.component';
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule {}
