d9b7e9d3a85124a83dc1cb6f67c81d2c4feb65db
luissalvado <luismiguelsalvado@gmail.com>
2020-10-09 17:05:54 +0100

    main ts

---
+++ main.js
@@ -0,0 +1,19 @@
+System.register(["./app/app.module", "@angular/platform-browser-dynamic"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var app_module_1, platform_browser_dynamic_1;
+    return {
+        setters: [
+            function (app_module_1_1) {
+                app_module_1 = app_module_1_1;
+            },
+            function (platform_browser_dynamic_1_1) {
+                platform_browser_dynamic_1 = platform_browser_dynamic_1_1;
+            }
+        ],
+        execute: function () {
+            platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+        }
+    };
+});
+//# sourceMappingURL=main.js.map
